1fb33a8612310dcfe9a83c016c22bea1909c9bd6
jlord <jaylordbucayu@gmail.com>
2024-10-16 09:56:47 +0800

    try potek

--- middleware.ts
+++ middleware.ts
@@ -24,7 +24,6 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 }
-
 export const config = {
   matcher: ['/', '/predictions'],
 };
