e25f2c1bf43452b9746b8e0e3bdd04d861307df3
jlord <jaylordbucayu@gmail.com>
2024-11-05 22:17:00 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -8,18 +8,22 @@ export async function middleware(req: NextRequest) {
     const token = await getToken({ req });
     const { pathname } = req.nextUrl;
 
+
+    if (token && pathname === '/') {
+        console.log('sdd')
+        return NextResponse.next();
+      }
+
+      
     if (!token && pathname === '/predictions') {
       return NextResponse.redirect(new URL('/', req.url));
     }
     // If the user is logged in and tries to visit the login page, redirect to /predictions
-    if (token) {
-      return NextResponse.redirect(new URL('/predictions', req.url));
-    }
-
+   
     // If the user is not logged in and tries to access protected routes, redirect to login page
    
 
-    return NextResponse.next();
+    return  NextResponse.next();
   } catch (error) {
     console.error('Middleware error:', error);
     return NextResponse.redirect(new URL('/', req.url));
